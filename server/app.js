4d6a6363f1a8b977feb5496d1bc7a9504722e132
EmeraldCoder <la_plume_en_or@hotmail.com>
2013-02-16 11:05:01 -0500

    demo nodejs

---
+++ server/app.js
@@ -0,0 +1,38 @@
+var app = require('http').createServer(httpHandler),
+    io = require('socket.io').listen(app),
+    fs = require('fs');
+
+/*
+ * Start our server
+ */
+app.listen(8080);
+console.log('Server started on 127.0.0.1:8080');
+
+/*
+ * Http server stuff
+ * send our html5 client to the browser
+ */
+function httpHandler (req, res) {
+    fs.readFile(__dirname + '/../client/index.html', function(err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+/*
+ * WebSocket server stuff
+ */
+io.on('connection', function (socket) {
+    // event call we the client ask a question
+    socket.on('question', function (data) {
+        console.log('Question received', data);
+        // send the answer to the question to the client
+        socket.emit('answer', { answer: 'Demande à Chuck Norris' });
+    });
+});
+

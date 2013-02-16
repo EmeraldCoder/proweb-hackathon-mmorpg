095b0f5067cbdd86e3b755886c51ee4ed8f956dd
Pier-Olivier Champagne <pierolivierchampagne@gmail.com>
2013-02-16 13:48:30 -0500

    test

--- server/app.js
+++ server/app.js
@@ -1,3 +1,45 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("PANDA");
+});
+
+var kittySchema = mongoose.Schema({
+    name: String
+})
+
+var Kitten = mongoose.model('Kitten', kittySchema)
+
+var silence = new Kitten({ name: 'Silence' })
+console.log(silence.name) // 'Silence'
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name"
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema)
+
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak() // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+  if (err) // TODO handle the error
+  fluffy.speak();
+});
+
+Kitten.find(function (err, kittens) {
+  if (err) // TODO handle err
+  console.log(kittens)
+})
+
+Kitten.find({ name: /^Fluff/ }, callback)
+
 var app = require('http').createServer(httpHandler),
     io = require('socket.io').listen(app),
     fs = require('fs');
@@ -35,4 +77,6 @@ io.on('connection', function (socket) {
         socket.emit('answer', { answer: 'Demande à Chuck Norris' });
     });
 });
+
+
 
